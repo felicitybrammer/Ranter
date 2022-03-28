0647adbd5b081f2673fa3df799201940fb9d0e16
Felicity Brammer <felicitybrammer@gmail.com>
2022-03-28 15:44:38 -0700

    heroku deployment

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const session = require('express-session')
 const SequelizeStore = require('connect-session-sequelize')(session.Store)
 
 const sess = {
-    secret: 'secretidhere',
+    secret: process.env.SECRET,
     cookie: {},
     resave: false,
     saveUninitialized: false,
